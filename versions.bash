a81491d4c8af8194fdd5e55da419b89da9aa6ee1
WillForan <willforan@gmail.com>
2023-10-24 11:09:12 -0400

    🐛 versions.bash: go and rust

--- versions.bash
+++ versions.bash
@@ -3,3 +3,5 @@ julia --version
 python -c 'import nibabel,numpy,sys; print(f"Python {sys.version}; np {numpy.__version__}; nib {nibabel.__version__}")'; 
 octave --version|sed 1q
 R --version|sed 1q
+go version
+rustc --version
