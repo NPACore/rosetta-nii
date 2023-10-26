eda2160881cba990a6a374cc9e7f68724ad0a072
Will Foran <willforan@gmail.com>
2023-10-26 09:23:51 -0400

    🐛 julia: like rust, silent overflow error with large sum. scale to fix

--- scripts/niimean.jl
+++ scripts/niimean.jl
@@ -2,4 +2,7 @@
 using Statistics
 using NIfTI
 ni = niread("wf-mp2rage-7t_2017087.nii.gz");
-print(mean(ni))
+print(mean(ni/1000)*1000)
+# sum of 'ni' too large for float64 type! scale to avoid overflow
+# w/o scalling mean reports 836.149 instead of actual 838.206399
+# much slower alternative: mean(BigFloat,ni)
